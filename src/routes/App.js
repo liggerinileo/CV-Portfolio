24b80b85aae34e19244fcb155c3911531cb8fbae
Leandro Liggerini <liggerinileo@gmail.com>
2020-10-09 21:47:51 -0300

    Probando Rutas

--- src/routes/App.js
+++ src/routes/App.js
@@ -14,6 +14,7 @@ const App = () => (
             <Route exact path='/education' component={Education} />
             <Route exact path='/portfolio' component={Portfolio} />
             <Route exact path='/courses' component={Courses} />
+            <Route component={Home} />
         </Switch>
     </BrowserRouter>
 )
