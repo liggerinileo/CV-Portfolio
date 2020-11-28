9105393ac953d408e32361d26254949e4f224980
Leandro Liggerini <liggerinileo@gmail.com>
2020-11-28 01:11:03 -0300

    Updates

---
+++ precache-manifest.124e4efd1490fe021ce823aeeefa7b01.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92b49831a8a2f940a96bec91fcdd823",
+    "url": "/CV-Portfolio/index.html"
+  },
+  {
+    "revision": "82b73038c6067b48892f",
+    "url": "/CV-Portfolio/static/css/main.f7b7afef.chunk.css"
+  },
+  {
+    "revision": "a3f45950a99f0149263a",
+    "url": "/CV-Portfolio/static/js/2.bc96f342.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CV-Portfolio/static/js/2.bc96f342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b73038c6067b48892f",
+    "url": "/CV-Portfolio/static/js/main.43f4d69b.chunk.js"
+  },
+  {
+    "revision": "cd7c59da82313620e40e",
+    "url": "/CV-Portfolio/static/js/runtime-main.4ffba8fb.js"
+  },
+  {
+    "revision": "2e49fdd0cfc5237dc4a006eb5f3f9994",
+    "url": "/CV-Portfolio/static/media/bachelors-degree.2e49fdd0.svg"
+  },
+  {
+    "revision": "ee195ea83d87b91a38e98804eecd2c55",
+    "url": "/CV-Portfolio/static/media/city.ee195ea8.svg"
+  },
+  {
+    "revision": "a67879acae2902c1b0f873d6009acfe7",
+    "url": "/CV-Portfolio/static/media/email.a67879ac.svg"
+  },
+  {
+    "revision": "12c50aa54686093063deb576d53d7e6e",
+    "url": "/CV-Portfolio/static/media/fondo.12c50aa5.png"
+  },
+  {
+    "revision": "13030725a3439b51ae7943033399585d",
+    "url": "/CV-Portfolio/static/media/github-brands.13030725.svg"
+  },
+  {
+    "revision": "94e1436072a6ac24d4b52401278123c2",
+    "url": "/CV-Portfolio/static/media/jsUdemy.94e14360.jpg"
+  },
+  {
+    "revision": "4935fca7fcca1b7cba630c07844738a3",
+    "url": "/CV-Portfolio/static/media/jspro.4935fca7.jpg"
+  },
+  {
+    "revision": "55a1cf87d75c8a81afdd8de636174b65",
+    "url": "/CV-Portfolio/static/media/libros.55a1cf87.svg"
+  },
+  {
+    "revision": "784614ff4272d7b589eb144807adf528",
+    "url": "/CV-Portfolio/static/media/linkedin.784614ff.svg"
+  },
+  {
+    "revision": "15b5f642b442fecba6dab8ccc8c57fa4",
+    "url": "/CV-Portfolio/static/media/movies.15b5f642.png"
+  },
+  {
+    "revision": "3f273a1818e3b4dcd2b3074ed31da492",
+    "url": "/CV-Portfolio/static/media/reactNative.3f273a18.jpg"
+  },
+  {
+    "revision": "ab247ddbf087be7a2a26f667cb3c3e95",
+    "url": "/CV-Portfolio/static/media/reactPractico.ab247ddb.jpg"
+  },
+  {
+    "revision": "4c2ebe9ba14d03a8da6882d0887500de",
+    "url": "/CV-Portfolio/static/media/reactRouter.4c2ebe9b.jpg"
+  },
+  {
+    "revision": "ae44aad04b92c0fcc48e9a4b71a6de99",
+    "url": "/CV-Portfolio/static/media/reactjs.ae44aad0.jpg"
+  },
+  {
+    "revision": "697376a6d2643a47863455482435ff44",
+    "url": "/CV-Portfolio/static/media/redux.697376a6.jpg"
+  },
+  {
+    "revision": "ccf522aae5a7cec5d7fefea17773f99d",
+    "url": "/CV-Portfolio/static/media/routerRedux.ccf522aa.jpg"
+  },
+  {
+    "revision": "6fea9e0f7acfbcd038e517a6ec99232b",
+    "url": "/CV-Portfolio/static/media/rs.6fea9e0f.jpg"
+  },
+  {
+    "revision": "5cb91de52684f975fa8830ddbf4daf49",
+    "url": "/CV-Portfolio/static/media/school.5cb91de5.svg"
+  },
+  {
+    "revision": "598fd0316831a5080f45ce7f393aaf45",
+    "url": "/CV-Portfolio/static/media/student.598fd031.svg"
+  },
+  {
+    "revision": "8d6ada22d040fe25016773062b2781d8",
+    "url": "/CV-Portfolio/static/media/testing.8d6ada22.jpg"
+  },
+  {
+    "revision": "38ca0ead98a00e42ee67bdeac6191f4e",
+    "url": "/CV-Portfolio/static/media/travel.38ca0ead.jpg"
+  },
+  {
+    "revision": "f83a8a21a9cf2031ec043f62e7ed033d",
+    "url": "/CV-Portfolio/static/media/web.f83a8a21.svg"
+  }
+]);
